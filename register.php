3c98e248128a9d7afda93b641f733f5098a8ea7a
snails <snailsqz@gmail.com>
2024-03-09 14:28:53 +0700

    s

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/register.css">
-    <title>Register</title> <!-- Morgana -->
+    <title>Register</title> <!-- Morgana is boss main-->
     <style>
         .password{
            -webkit-text-security:disc;
