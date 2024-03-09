ef3767d3cf8775e422ca0e41917b7c0837af4e57
Sencoool <s6506021610037@kmutnb.ac.th>
2024-03-09 14:26:13 +0700

    Morgana Test

--- create.php
+++ create.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/create.css">
-    <title>Movie Create</title>
+    <title>Movie Create</title> <!-- Morgana -->
 </head>
 <?php
     $hostname = "localhost";
